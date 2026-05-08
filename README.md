3a0aa154295d8bc115fcac0ddf2d1717195aba81
delmas cox <delmaslcox1@gamil.com>
2026-05-08 09:36:57 -0400

    fixed moible side

--- README.md
+++ README.md
@@ -1,21 +1,7 @@
-<!-- need to add header to saved articles -->
+link for Video -
 
-<!-- add cards styles to srticle cards and buttons  -->
-<!-- add the delete buttin and bookmark -->
-<!-- also add the keywords too -->
+all tools I used for the project 
 
-<!-- style the mobile side  -->
+hard parts of the project 
 
-<!-- add the sorry nothing found when search -->
 
-<!-- add the succesful sumbited button  -->
-
-<!-- make sure the highlight part on the botton the the header change depending on the page the user is on  -->
-
-<!-- add the log out button inside the user button -->
-
-<!-- have the preload when search -->
-
-<!-- have the show more button work -->
-
-<!-- add the dicord and linkin button to footer -->
--- news_explorer_frontend/src/components/Footer/Footer.jsx
+++ news_explorer_frontend/src/components/Footer/Footer.jsx
@@ -5,39 +5,41 @@ function Footer() {
     <footer className="footer">
       <p className="footer__copyright">© 2026 Supersite, Powered by News API</p>
 
-      <div className="footer__links">
-        <a className="footer__link" href="/">
-          Home
-        </a>
+      <div className="footer__navigation">
+        <div className="footer__links">
+          <a className="footer__link" href="/">
+            Home
+          </a>
 
-        <a
-          className="footer__link"
-          href="https://tripleten.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          TripleTen
-        </a>
-      </div>
+          <a
+            className="footer__link"
+            href="https://tripleten.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            TripleTen
+          </a>
+        </div>
 
-      <div className="footer__socials">
-        <a
-          className="footer__icon-link"
-          href="https://github.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src="/github.svg" alt="GitHub" className="footer__icon" />
-        </a>
+        <div className="footer__socials">
+          <a
+            className="footer__icon-link"
+            href="https://github.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/github.svg" alt="GitHub" className="footer__icon" />
+          </a>
 
-        <a
-          className="footer__icon-link"
-          href="https://linkedin.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src="/linked-in.svg" alt="LinkedIn" className="footer__icon" />
-        </a>
+          <a
+            className="footer__icon-link"
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/linked-in.svg" alt="LinkedIn" className="footer__icon" />
+          </a>
+        </div>
       </div>
     </footer>
   );
