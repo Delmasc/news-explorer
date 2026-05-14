9c8b69fb6d9e4a404bfb1f43b67c29fc93e0ec42
delmas cox <delmaslcox1@gamil.com>
2026-05-14 13:24:52 -0400

    fixed main.jsx

--- news_explorer_frontend/src/main.jsx
+++ news_explorer_frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./components/App/App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/news-explorer">
       <App />
